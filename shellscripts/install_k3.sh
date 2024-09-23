bed6bab52de95e18920727f372f6e152b0ff4500
Irwin-rex <irwin.a@equipohealth.com>
2024-09-23 20:06:43 +0530

    updated k3 script

--- shellscripts/install_k3.sh
+++ shellscripts/install_k3.sh
@@ -3,7 +3,6 @@
 # Download and install K3s
 curl -sfL https://get.k3s.io | sh -s - --disable traefik,metrics-server
 
-
 # Wait for K3s to start
 echo "Waiting for K3s to start..."
 sleep 20
@@ -11,15 +10,17 @@ sleep 20
 # Create .kube directory if it doesn't exist
 mkdir -p ~/.kube/
 
-# Create the kube config file
-touch ~/.kube/config
+# Remove existing kube config file if it exists
+if [ -f ~/.kube/config ]; then
+    rm ~/.kube/config
+fi
+
+# Create a symbolic link to the K3s kubeconfig file
+ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
 
 # Change Permission
-chmod 600 ~/.kube/config
-chmod 700 /etc/rancher/k3s/k3s.yaml
-
-# Copy the K3s kubeconfig file to the .kube/config file
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chmod 600 ~/.kube/config
+sudo chmod 700 /etc/rancher/k3s/k3s.yaml
 
 # Verify K3s installation
 sudo k3s kubectl get nodes
