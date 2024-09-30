0467875acadc0452642e7c2784cd8329474370b7
Irwin-rex <irwin.a@equipohealth.com>
2024-09-30 18:43:08 +0530

    updated all.yml and install k3s

--- shellscripts/install_k3s.sh
+++ shellscripts/install_k3s.sh
@@ -50,33 +50,13 @@ echo 'Wait for 30 Seconds'
 
 sleep 30
 
-# Detect the latest Cilium CLI version
-CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+# Installing Cilium by helm
 
-# Detect system architecture (amd64 or arm64)
-CLI_ARCH=amd64
-if [ "$(uname -m)" = "aarch64" ]; then
-  CLI_ARCH=arm64
-fi
+helm repo add cilium https://helm.cilium.io/
+helm repo update
 
-# Download the Cilium CLI binary and checksum
-curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+helm install cilium cilium/cilium --version 1.16.2 \
+   --namespace kube-system \
+   --set operator.replicas=1
 
-# Verify checksum
-sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
-
-# Extract Cilium CLI to /usr/local/bin
-sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
-
-# Cleanup downloaded files
-rm -rvf cilium-linux-*.tar.gz.sha256sum
-rm -rvf cilium-linux-*.tar.gz
-
-# Install Cilium using the k3s default podCIDR
-cilium install \
-    --set ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16" \
-    --set global.tunnel="vxlan" \
-    --set trafficPolicy.enable=true \
-    --set trafficPolicy.defaultIngressPolicy="allow" \
-    --set trafficPolicy.defaultEgressPolicy="allow"
 
